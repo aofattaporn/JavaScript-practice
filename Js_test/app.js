3cd6597b93b074921126d4d79f22c308dec0c120
aofattapoorn <aofattapon321@gmail.com>
2021-01-05 01:49:39 +0700

    initail Js-learn form born to dev

---
+++ Js_test/app.js
@@ -0,0 +1,39 @@
+// I can show a message in an alert box by the alert() command 
+// alert("Hello Attaporn")
+
+//-> I can show a message by String in the function console.log
+console.log("Hello i na mha")
+
+var x = "Hello i na mha"
+console.log(x)
+console.log(x)
+
+// I can change the value without declaring a new variable.
+x = "Chang data"
+console.log(x)
+
+var Firstname = "Attaporn\t"
+var Lastname = "Peungsook"
+console.log(Firstname+Lastname)
+
+// I can check type of data by typeof command 
+typeof Firstname
+console.log(typeof Firstname)
+
+// I cann't use Sting multiply by integer 
+// that show a NaN is mean "Not a Number"
+console.log("ff"*2)
+
+// Js block scape 
+{    
+    var num1 = 10 
+    let num2 = 15 
+
+}
+console.log(num1)
+// console.log(num2) --> you cann't use value form let command on out scorp
+
+// you can input massage by prompt command
+// prompt("เฮ้ยๆๆๆๆ กรอกชื่อมาหน่อยซิ")
+// var name = prompt("เฮ้ยๆกรอกชื่อมาหน่อยซิ")
+// console.log(name)
---
+++ Js_test/random_number.js
@@ -0,0 +1,8 @@
+let lotto_number = prompt("กรอกเลขที่ต้องการ :")
+console.log(lotto_number)
+
+lotto_random = Math.floor(Math.random * 10)
+console.log(lotto_random)
+
+
+
