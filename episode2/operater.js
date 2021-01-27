95872ce51b5f5d7e6de82435354f154e6f5b51de
aofattapoorn <aofattapon321@gmail.com>
2021-01-27 23:22:36 +0700

    learn operators in js

---
+++ episode2/operater.js
@@ -0,0 +1,26 @@
+// note : นิพจน์ 1 ตัวประกอบด้วย operators และ operand 
+
+// note : operators ทางคณิตศาสตร์
+// todo Arithmetic Operators 
+// * เมื่อใช้ operators กับ NaN กับ Infinity ได้ค่าเดิม 
+// ! เช่น + - * / % ++ -- (+) (-)
+console.log("===========Arithmetic Operators=============="); 
+console.log("100 - true :",100 - true); 
+var a = false 
+console.log("false++ :", (++a));
+console.log("");
+
+
+console.log("===========Assignment Operators==============");
+// ! == += -= *= /= %=
+console.log("เป็น operators ที่กำหนดค่าให้กับตัวแปร \n\n");
+
+console.log("===========String Operators==============");
+// ! + +=  ~Arithmetic Operators 
+a = "foo" + "bar";
+b = "foo" - 10;
+c = "10" - 10;
+console.log(a);
+console.log("\" foo \" - 10 :", b);
+console.log("\" 10 \" - 10 :", c);
+
